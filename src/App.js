101466f75607856fc36cee94d005a5278fcffcde
mannan <abdulmannan.abbasi@gmail.com>
2018-06-19 13:18:36 +0100

    getweather function  calling api and logging data to console +prop call

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import Titles from "./components/Titles"
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "ccde404ff23089803dafa662fd612570";
 
 class App extends React.Component{
+  getWeather = async (e)=>{
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
+
   render(){
     return(
 
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather = {this.getWeather}/>
         <Weather/>
 
       </div>

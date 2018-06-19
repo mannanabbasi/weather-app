088e06c38f69c6a048d07ec060ebec3fa29cd564
mannan <abdulmannan.abbasi@gmail.com>
2018-06-19 20:48:48 +0100

    Modified getWeather(), added city and country placeholder to fetch()

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ const API_KEY = "ccde404ff23089803dafa662fd612570";
 class App extends React.Component{
   getWeather = async (e)=>{
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
   }

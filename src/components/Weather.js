ea8e6d70d9e96598f3077c3b97f29de98cdaff47
mannan <abdulmannan.abbasi@gmail.com>
2018-06-20 11:05:53 +0100

    Weather component modified, displays all props in fields required

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,10 @@ class Weather extends React.Component{
     render(){
         return(
             <div>
-                <p>This is weather componet </p>
+                {this.props.city && this.props.country && <p>Location : {this.props.city}, {this.props.country}</p>}
+                {this.props.temperature && <p>Temperature : {this.props.temperature}</p>}
+                {this.props.humidity && <p> Humidity : {this.props.humidity}</p>}
+                {this.props.description && <p> Condition : {this.props.description}</p>}
             </div>
 
         );

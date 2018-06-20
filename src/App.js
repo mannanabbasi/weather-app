b659ef6b903ee6d66f75dbfb13bcd589acc13778
mannan <abdulmannan.abbasi@gmail.com>
2018-06-20 10:29:28 +0100

    added comments to getWeather function

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ class App extends React.Component{
   getWeather = async (e)=>{
     e.preventDefault();
 
+    //get city and country element's value from the form component
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
+    
+    //make call to api
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+
+    //wait for the call to complete and store its value in data
     const data = await api_call.json();
+    
     console.log(data);
   }
 

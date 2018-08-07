e9f66e4b8041809384c552b2e8b493ca5d640e82
mannan <abdulmannan.abbasi@gmail.com>
2018-08-07 13:33:03 +0100

    remove this after modifying component to functional

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React from "react";
+import '../form.css';
 
 const Form = props => (
             
-    <form onSubmit = {props.getWeather}>
+    <form className = "userInput" onSubmit = {props.getWeather}>
         <input type = "text" name = "city" placeholder = "City.."/>
         <input type = "text" name = "country" placeholder = "Country.."/>
         <button> Get Weather </button>    

07c3d61af01b7a750889116ca7d92879b77f497a
mannan <abdulmannan.abbasi@gmail.com>
2018-08-08 21:46:03 +0100

    Weather icon added

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,6 +5,8 @@ const Weather = props => (
     <div className = "dashboard">
         {props.city && props.country && <h1>{props.city}, {props.country}</h1>}
         {props.temperature && <p><span id = "temp"> {props.temperature}°C </span></p>}
+        <img className="img" src={`http://openweathermap.org/img/w/${props.icon}.png`}/>
+
         {props.description && <p><span id = "condition"> {props.description}</span></p>}
         {props.error && <p>{props.error}</p>}    
     </div>

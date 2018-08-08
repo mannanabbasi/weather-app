ad306f0761b57c9f1a86b0869edd40f709f94f37
mannan <abdulmannan.abbasi@gmail.com>
2018-08-08 13:13:44 +0100

    removed humidity and modified props to add span ids

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,12 @@
 import React from "react";
+import '../weather.css';
 
 const Weather = props => (
-    <div>
-        {props.city && props.country && <p>Location : {props.city}, {props.country}</p>}
-        {props.temperature && <p>Temperature : {props.temperature}</p>}
-        {props.humidity && <p> Humidity : {props.humidity}</p>}
-        {props.description && <p> Condition : {props.description}</p>}
-        {props.error && <p>{props.error}</p>}
+    <div className = "dashboard">
+        {props.city && props.country && <h1>{props.city}, {props.country}</h1>}
+        {props.temperature && <p><span id = "temp"> {props.temperature}°C </span></p>}
+        {props.description && <p><span id = "condition"> {props.description}</span></p>}
+        {props.error && <p>{props.error}</p>}    
     </div>
 
 )

b99492c19e652e840f3fbe899a64bdf8eb0d4ed0
mannan <abdulmannan.abbasi@gmail.com>
2018-08-11 09:04:21 +0100

    changed icon position

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,9 +4,10 @@ import '../weather.css';
 const Weather = props => (
     <div className = "dashboard">
         {props.city && props.country && <h1>{props.city}, {props.country}</h1>}
-        {props.temperature && <p><span id = "temp"> {props.temperature}°C </span></p>}
-        <img className="img" src={`http://openweathermap.org/img/w/${props.icon}.png`}/>
-
+        {props.temperature && <p><span id = "temp"> {props.temperature}°C</span> <img className="img" src={`http://openweathermap.org/img/w/${props.icon}.png`}/>
+</p>}
+        
+        
         {props.description && <p><span id = "condition"> {props.description}</span></p>}
         {props.error && <p>{props.error}</p>}    
     </div>

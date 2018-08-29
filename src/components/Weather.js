24b989ef7c51e93c3acc2696d99fc541c519c0bf
mannan <abdulmannan.abbasi@gmail.com>
2018-08-29 20:48:26 +0100

    only render dashboard if component has loaded

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,24 @@
 import React from "react";
 import '../weather.css';
 
-const Weather = props => (
-    <div className = "dashboard">
+const Weather = (props) => {
+    
+    if(props.hasLoaded){
+        return ( <div className = "dashboard">
         {props.city && props.country && <h1>{props.city}, {props.country}</h1>}
-        {props.temperature && <p><span id = "temp"> {props.temperature}°C</span> <img className="img" src={`http://openweathermap.org/img/w/${props.icon}.png`}/>
+        {props.temperature && <p><span id = "temp"> {props.temperature}°C</span> <img className="img" src={`http://openweathermap.org/img/w/${props.icon}.png`} alt = "weathericon"/>
 </p>}
         
         
         {props.description && <p><span id = "condition"> {props.description}</span></p>}
-        {props.error && <p>{props.error}</p>}    
+           
     </div>
+    )
+    }
+    else{
+        return(<div>{props.error && <p>{props.error}</p>}</div> )
+    }
 
-)
+}
 
 export default Weather;
